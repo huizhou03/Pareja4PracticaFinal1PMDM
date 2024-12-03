b5b0a375c11f5cc1f8366c34507a20da10910922
Huihui <huiz0517@gmail.com>
2024-12-03 22:54:08 +0100

    MainActivity 1 hecho

--- app/src/main/java/com/example/pareja4practicafinal1pmdm/MainActivity.java
+++ app/src/main/java/com/example/pareja4practicafinal1pmdm/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.pareja4practicafinal1pmdm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textViewTitular;
+    private TextView textView2Nombre;
+    private Button botonCrear;
+    private EditText editTextTextNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textViewTitular = findViewById(R.id.TextViewTitular);
+        textView2Nombre = findViewById(R.id.TextView2Nombre);
+        botonCrear = findViewById(R.id.BotonCrear);
+        editTextTextNombre = findViewById(R.id.EditTextTextNombre);
+    }
+    public void crearPersonaje(View view){
+        Intent intent = new Intent();
+        intent.putExtra("NombreAlumno", editTextTextNombre.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
