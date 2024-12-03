922d3bf108b67a13f2ff4cb82119cf6395967377
Anton Luo <22301690@live.uem.es>
2024-12-03 23:46:38 +0100

    avance en el la pestaña 2

--- app/src/main/java/com/example/pareja4practicafinal1pmdm/MainActivity2.java
+++ app/src/main/java/com/example/pareja4practicafinal1pmdm/MainActivity2.java
@@ -1,6 +1,12 @@
 package com.example.pareja4practicafinal1pmdm;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+    private Spinner clases;
+    private int[] imagenesJobs = {R.drawable.mago, R.drawable.brujo, R.drawable.explorador, R.drawable.monje, R.drawable.hechicero};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,30 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        clases = findViewById(R.id.Clases);
+        String[] jobs = {"Mago", "Brujo", "Explorador", "Monje", "Hechicero", "Barbaro", "Guerrero", "Paladín", "Druida", "Bardo", "Clerigo", "Pícaro"};
+
+    }
+
+    class ClasesAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
