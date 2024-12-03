d43c4bca1158b8cd8073c719003e2194af359633
Huihui <huiz0517@gmail.com>
2024-12-03 23:08:06 +0100

    Actualizacion MainActivity 1

--- app/src/main/java/com/example/pareja4practicafinal1pmdm/MainActivity.java
+++ app/src/main/java/com/example/pareja4practicafinal1pmdm/MainActivity.java
@@ -7,18 +7,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textViewTitular;
-    private TextView textView2Nombre;
+    private TextView pantalla1;
+    private TextView titulo;
     private Button botonCrear;
-    private EditText editTextTextNombre;
+    ActivityResultLauncher<Intent> startForLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            String nombre = result.getData().getStringExtra("nombreDato");
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +40,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        textViewTitular = findViewById(R.id.TextViewTitular);
-        textView2Nombre = findViewById(R.id.TextView2Nombre);
+
+        titulo = findViewById(R.id.TextViewTitular);
+        pantalla1 = findViewById(R.id.TextView2Nombre);
         botonCrear = findViewById(R.id.BotonCrear);
-        editTextTextNombre = findViewById(R.id.EditTextTextNombre);
+
+        String nombre = getIntent().getStringExtra("NombreAutor");
+        if(nombre == null){
+            Toast.makeText(this, "Por favor, introduce un nombre", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Bienvenido " + nombre, Toast.LENGTH_SHORT).show();
+        }
     }
     public void crearPersonaje(View view){
-        Intent intent = new Intent();
-        intent.putExtra("NombreAlumno", editTextTextNombre.getText().toString());
-        setResult(RESULT_OK, intent);
-        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        startForLauncher.launch(intent);
     }
 }
